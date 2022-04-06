c4a0b633e2ef7e4f01a6cc2f7deb4bd138a2f3a3
Febe Calderon <samcalma1@gmail.com>
2022-04-06 17:04:31 +0200

    vue cli first + bonus

---
+++ src/components/Hero.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="hello">
+    <h2>Sono Hero</h2>
+    <img src="../assets/logo.png" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeroComponent",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h2 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
